415c41edb3e70f7dfb0f92873ce237e201d197d1
Daniel Cambria <daniel.cambria@bureau-it.com>
2022-01-26 02:46:04 -0300

    v1.12

--- hextodec.sh
+++ hextodec.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # APP NAME: hextodex.sh - Hex to Dec 
-# DESCRIPTION: Criado para facilitar a conversão de bytes escritos em hexadecimal em números decimais. A entrada é non-sensitive.
+# DESCRIPTION: Criado para facilitar a conversão de bytes escritos em hexadecimal em números decimais. A entrada é non-sensitive. Utiliza a calculadora BC para a conversão.
 #
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # AUTHOR NAME: Daniel Cambría
@@ -20,23 +20,13 @@
 # VERSÃO 1.0: Leitura de vários blocos de 1 byte separados por espaços (-b) e números maiores longos sem espaços.
 # VERSÃO 1.1: Incorpora supressão de saída hexadecimal
 # VERSÃO 1.11: Tratamento de entrada. Evita hexadecimais impossíveis e sintaxe de comando.
+# VERSÃO 1.12: Melhorias de código
 #
 # TODO: 
 #
 #
 #
 
-hextodec(){
-    NORMALIZE=$(echo $@ | tr '[:lower:]' '[:upper:]' | sed "s/ //g")
-
-    if [[ $NOHEX != 1 ]]
-    then
-        echo -en "\033[1;4;31m$NORMALIZE \033[0m"
-    fi
-
-    DEC=$(bc <<< "ibase=16;$NORMALIZE")
-    printf '\033[4;41m%3d\033[0m' $DEC
-}
 
 banner() {
     echo "hextodec.sh"
@@ -52,17 +42,38 @@ banner() {
     echo ""
     echo "- Use -b para leitura de bytes separados por espaços. Ex. FF FF FF"
     echo "- Use -n para exibir somente o resultado decimal."
-} 
+    echo ""
+}
 
-echo ""
 
+erro_generico() {
+    echo "Valor inválido."
+    echo "Confira o número hexadecimal ou a sintaxe do programa."
+    echo ""
+}
+
+
+hextodec(){
+    NORMALIZE=$(echo $@ | tr '[:lower:]' '[:upper:]' | sed "s/ //g")
+
+    if [[ $NOHEX != 1 ]]
+    then
+        echo -en "\033[1;4;31m$NORMALIZE \033[0m"
+    fi
+
+    DEC=$(bc <<< "ibase=16;$NORMALIZE")
+    printf '\033[4;41m%3d\033[0m' $DEC
+}
+
+
+# Sem argumento, exibe o banner
 if [[ $# -eq 0 ]] 
 then
     banner
     exit 0
 fi
 
-
+# Inicia loop de argumentos
 while test -n "$1"
 do
     case "$1" in
@@ -74,7 +85,8 @@ do
                  export HEX=$@
                  break 
              else
-                 echo "Valor inválido. Confira o número hexadecimal ou a sintaxe do programa."
+                 # avisa se digitou hex ou argumento incorreto
+                 erro_generico
                  exit 1
              fi
              ;;
@@ -83,6 +95,7 @@ do
     shift
 done
 
+# loop hex separados por espaço
 if [[ $ONEBITE == 1 ]]
 then
     for i in $HEX                    
@@ -91,5 +104,6 @@ then
        echo -n " "
     done
 else
-   hextodec $HEX 
+    # calculo total de hex, sem espaços
+    hextodec $HEX 
 fi
